14a2e00043a568278c2bf4b7cf9769b12739beff
LucasBonna <luicasbonnafavaro@gmail.com>
2024-10-25 17:06:40 -0300

    first commit

---
+++ internal/schemas/services.go
@@ -0,0 +1,15 @@
+package schemas
+
+type ServiceNames string
+
+const (
+	CFCrawler       ServiceNames = "cfcrawler"
+	CFStorage       ServiceNames = "cfstorage"
+	CFNotifications ServiceNames = "cfnotifications"
+)
+
+var ServiceUrls = map[ServiceNames]string{
+	CFCrawler:       "http://cfcrawler-api",
+	CFStorage:       "http://cfcstorage-api",
+	CFNotifications: "http://cfcnotifications-api",
+}
