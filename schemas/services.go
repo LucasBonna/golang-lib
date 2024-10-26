2f506b2cecda916f8c582a76e3b901de488b00eb
Lucas Bonna Favaro <88051985+LucasBonna@users.noreply.github.com>
2024-10-26 03:07:03 -0300

    feat: cfgateway

--- schemas/services.go
+++ schemas/services.go
@@ -3,12 +3,14 @@ package schemas
 type ServiceNames string
 
 const (
+	CFGateway       ServiceNames = "cfgateway"
 	CFCrawler       ServiceNames = "cfcrawler"
 	CFStorage       ServiceNames = "cfstorage"
 	CFNotifications ServiceNames = "cfnotifications"
 )
 
 var ServiceUrls = map[ServiceNames]string{
+	CFGateway:       "http://cfgateway-api",
 	CFCrawler:       "http://cfcrawler-api",
 	CFStorage:       "http://cfstorage-api",
 	CFNotifications: "http://cfcnotifications-api",
